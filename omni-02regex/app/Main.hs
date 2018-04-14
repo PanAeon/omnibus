0850ef6de7fba47fed743403e629db64eca36c22
PanAeon <vitalii.voloshyn@gmail.com>
2018-04-14 23:05:44 +0100

    merge before rec

--- omni-02regex/app/Main.hs
+++ omni-02regex/app/Main.hs
@@ -94,98 +94,110 @@ next' = do
 
 
 
-
-------------------------------------------------------------------------------
-
--- all right, now automata
-
--- definition is stupid? or not
-data State =   State (Maybe State) [(Char, State)]
-             | FinalState deriving (Show)
-
--- so automata, need to jump on next state,
--- which means I need to normalize states in case of |
--- so is (abc)*|(ab*)
--- equal (ab)*c*?
-
--- (abc)*|abd|abe
--- (abf)* | (abc)* | (ab) (d|e)
---
--- -a-> * -b-> * -f->
---             * -c->
---             * -d->
---             * -e->
-
-finalState = State  Nothing []
-errorState = State  Nothing []
-emptyState = State  Nothing []
-
--- FIXME: runAutomata' "a((abcd)*)a" "aa"
 stupdidParser x = maybe (error "can not parse") id  $ parseMaybe  (expr <* eof) x
 
--- FIXME: runAutomata' "a((ac)*)a" "aa"
--- problem:                |     ^
---                         |_____| (* should backtrack on not matched, how?)
+-----------------------------------------------------------------------------------------------------------------------
+-- all right, now automata
+-----------------------------------------------------------------------------------------------------------------------
 
-runAutomata :: State -> String -> Bool
-runAutomata (State _ []) _ = True
-runAutomata (State Nothing xs) "" = False
-runAutomata (State (Just es) _) "" =
-  let
-    drillDown (State _ []) = True
-    drillDown (State Nothing _) = False
-    drillDown (State (Just s) _) = drillDown s
-  in drillDown es
-runAutomata (State es xs) (y:ys) =
-  case find (\x -> fst x == y) xs of
-    (Just (c,s)) -> runAutomata s ys
-    Nothing -> maybe False (\es' -> runAutomata es' (y:ys)) es
+
+data Ref = Ref Char State
+         -- | RecRef Char State
+         -- | BckRef Char State
+         | EpsRef State
+
+
+data State =   State Bool [Ref]
+
+instance Show State where
+  show = printAutomata
+
+printAutomata :: State -> String
+printAutomata (State _ []) = "▣"
+printAutomata (State r xs) =
+  if r
+  then "{" ++ (concat . intersperse " ,") (printRef <$> xs) ++ "}"
+  else "[" ++ (concat . intersperse " ,") (printRef <$> xs) ++ "]"
+
+printRef :: Ref -> String
+printRef (Ref c (State True _)) = '\'':c:'\'':"??<--"
+printRef (Ref c s) = '\'':c:'\'':"->" ++ (printAutomata s)
+-- printRef (RecRef c s) = '\'':c:'\'':"➡" ++ (printAutomata s)
+-- printRef (BckRef c _) = '\'':c:'\'':"⬅" ++ "??"
+printRef (EpsRef s) = "⟿" ++ (printAutomata s)
+-- printRef (BckEpsRef s) = "⬅⟿" ++ "??"
+
+-- Careful with this one: buildFromString "(ab|ac|ae|af|a)a"
+buildFromString s = buildAutomata (State False []) (stupdidParser s)
+
+-- can switch first param for ref..
+buildAutomata :: State -> Expression -> State
+buildAutomata fs (Term c)  = State False [Ref c fs]
+buildAutomata fs (Next e1 e2) = let
+                                  fs' = buildAutomata fs e2
+                                  in buildAutomata fs' e1
+
+buildAutomata fs (Sum e1 e2) = let
+                                 s1 = buildAutomata fs e1
+                                 s2 = buildAutomata fs e2
+                               in mergeStates s1 s2
+buildAutomata fs (Many e)    = let
+                                 (State xs) = buildAutomata fs' e
+                                 -- xs' = markAsRec <$> xs -- here epsilon is an error
+                                 fs' = undefined-- fmap all next states to BckRef
+                              in  fs'
+-- for now only ref && epsref
+mergeStates :: State -> State -> State
+mergeStates (State []) (State []) = State []
+mergeStates (State xs) (State []) = State $ xs ++ [EpsRef (State [])]
+mergeStates (State []) (State ys) = State $ [EpsRef (State [])] ++ ys
+mergeStates (State xs) (State ys) = State $ xs'' ++ common ++ ys'' ++ epsilons
+  where
+     isEpsilon (EpsRef _) = True
+     isEpsilon _          = False
+     epsilons = take 1 (filter isEpsilon (xs ++ ys))
+     xs' = filter (not . isEpsilon) xs
+     ys' = filter (not . isEpsilon) ys
+     sameChar (Ref c1 _) (Ref c2 _) = c1 == c2
+     common = [Ref c1 (mergeStates s1 s2) | (Ref c1 s1) <- xs', (Ref c2 s2) <- ys', c1 == c2 ]
+     xs'' = deleteFirstsBy sameChar xs' ys' -- knowing that chars are unique
+     ys'' = deleteFirstsBy sameChar ys' xs'
+
+
+--
+--
+-- buildAutomata' :: Maybe State -> State -> Expression -> State
+-- buildAutomata' es fs (Term c) = State es [(c, fs)]
+-- buildAutomata' es fs (Next e1 e2) = let
+--                                   fs' = buildAutomata' es fs e2
+--                                   in buildAutomata' es fs' e1
+-- buildAutomata' es fs (Many e) = let
+--                              fs' = buildAutomata' (Just fs) fs' e
+--                              in  fs'
+--
+-- buildAutomata' es fs (Sum e1 e2) = let
+--                                 (State _ xs) = buildAutomata' es fs e1
+--                                 (State _ ys) = buildAutomata' es fs e2
+--                              in (State es $ xs ++ ys)-- need epsilon? or eliminate
+
+-- runAutomata :: State -> String -> Bool
+-- runAutomata (State _ []) _ = True
+-- runAutomata (State Nothing xs) "" = False
+-- runAutomata (State (Just es) _) "" =
+--   let
+--     drillDown (State _ []) = True
+--     drillDown (State Nothing _) = False
+--     drillDown (State (Just s) _) = drillDown s
+--   in drillDown es
+-- runAutomata (State es xs) (y:ys) =
+--   case find (\x -> fst x == y) xs of
+--     (Just (c,s)) -> runAutomata s ys
+--     Nothing -> maybe False (\es' -> runAutomata es' (y:ys)) es
 
 
 -- runAutomata' "(ab)|b" "abaabab" yep, matched first two
 
-runAutomata' e s = runAutomata (buildAutomata' Nothing finalState (stupdidParser e)) s
-
-
-buildAutomata :: Maybe State -> State -> Expression -> State
-buildAutomata es fs (Term c)  = State es [(c, fs)]
-buildAutomata es fs (Next e1 e2) = let
-                                  fs' = buildAutomata' es fs e2
-                                  in buildAutomata' es fs' e1
-
-buildAutomata es fs (Sum e1 e2) = let
-                                -- need to merge two graphs xs && ys
-                                (State _ xs) = buildAutomata' es fs e1
-                                (State _ ys) = buildAutomata' es fs e2
-                             in (State es $ xs ++ ys)
-
-merge :: State -> State -> State
-merge FinalState FinalState = FinalState
-merge (State _ xs) FinalState = (State (Just FinalState) xs)
-merge FinalState (State _ ys) = (State (Just FinalState) ys)
--- merge (State _ ((c1,s1):xs)) (State _ ((c2,s2):ys)) 
-          -- | c1 == c2 = State Nothing (merge s1 s2):
-merge (State _ xs) (State _ ys) = undefined
-  where
-    zs = intersectBy (\ x y -> fst x == fst y) xs ys
-
-
-buildAutomata' :: Maybe State -> State -> Expression -> State
-buildAutomata' es fs (Term c) = State es [(c, fs)]
-buildAutomata' es fs (Next e1 e2) = let
-                                  fs' = buildAutomata' es fs e2
-                                  in buildAutomata' es fs' e1
-buildAutomata' es fs (Many e) = let
-                             fs' = buildAutomata' (Just fs) fs' e
-                             in  fs'
-
-buildAutomata' es fs (Sum e1 e2) = let
-                                (State _ xs) = buildAutomata' es fs e1
-                                (State _ ys) = buildAutomata' es fs e2
-                             in (State es $ xs ++ ys)-- need epsilon? or eliminate
-
-
-
+-- runAutomata' e s = runAutomata (buildAutomata' Nothing finalState (stupdidParser e)) s
 
 main :: IO ()
 main = someFunc
